2bb8a92c621d4a6a3b543ed665c967dfaccaf069
Janhavii12 <janhaviitankar@gmail.com>
2024-03-13 12:00:21 +0530

    initial deployment

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,3 +1,4 @@
+'use client';
 import {
   UserGroupIcon,
   HomeIcon,
@@ -10,14 +11,18 @@ const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
     name: 'Invoices',
-    href: '/dashboard/invoices',
+    href: '/dashboard/invoice',
     icon: DocumentDuplicateIcon,
   },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Customers', href: '/dashboard/customer', icon: UserGroupIcon },
 ];
+import { usePathname } from 'next/navigation';
 
 export default function NavLinks() {
+  const pathname= usePathname();
+  
   return (
+    
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
